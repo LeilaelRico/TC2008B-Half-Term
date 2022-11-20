8d201bbb30edc45fab8bb6351e3c1c488dafde10
LeilaelRico <A01707023@tec.mx>
2022-11-20 12:48:40 -0600

    Added Scenes and Delay Script.

---
+++ Graphics/Assets/Scripts/Delay.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using UnityEngine.SceneManagement;
+
+public class Delay : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        DOVirtual.DelayedCall(10, GotoNextScene);
+    }
+
+    // Update is called once per frame
+    void GotoNextScene()
+    {
+        SceneManager.LoadScene("Attack 3");
+        
+    }
+}
